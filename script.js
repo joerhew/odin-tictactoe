1c000bfecd57656396a58465831c2557b83ef781
joerhew <joerhew@gmail.com>
2023-03-15 16:50:22 -0400

    this branch is buggy; doesn't announce the winner, and there's an infinite loop when checking for a tie

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-const game = (() => {
+const gameBoard = (() => {
   // Create the 3x3 game board structure
-  const gameBoard = [
+  const board = [
     [undefined, undefined, undefined],
     [undefined, undefined, undefined],
     [undefined, undefined, undefined],
   ];
 
   // Place a player's marker on the cell of their choice
-  const updateGameBoard = (playerMarker, row, column) => {
-    gameBoard[row][column] = playerMarker;
+  const update = (playerMarker, row, column) => {
+    board[row][column] = playerMarker;
   };
 
   // Show the current game board
-  const showGameBoard = () => gameBoard;
+  const show = () => board;
+
+  const clear = () => {
+    for (let i = 0; i < board.length; i += 1) {
+        for (let j = 0; j < board[i].length; j += 1) {
+            board[i][j] = undefined;
+        }
+    }
+  }
 
   // Module returns the following functions
   return {
-    showGameBoard,
-    updateGameBoard,
+    show,
+    update,
+    clear
   };
 })();
 
@@ -27,17 +36,20 @@ const playerFactory = (name, marker) => ({
   marker,
 });
 // Create 2 players, each with their unique marker
+const players = [];
+
 const playerOne = playerFactory('Joe', 'O');
 const playerTwo = playerFactory('Felix', 'X');
-const players = [playerOne, playerTwo];
+
+players.push(playerOne);
+players.push(playerTwo);
 
 // Create a game controller object as a module
 const gameController = (() => {
   let whoseTurn;
-  // Check if the next turn can be taken, i.e. an empty cell is available
 
-  const checkGameStatus = () => {
-    const currentBoard = game.showGameBoard();
+  const checkStatus = () => {
+    const currentBoard = gameBoard.show();
     const possibleWinCombinations = [];
     const gameStatus = {
       winner: undefined,
@@ -45,11 +57,11 @@ const gameController = (() => {
 
     // Check if there's a tie
     const checkForTie = () => {
-      for (let row = 0; row < currentBoard.length; row++) {
-        for (let col = 0; col < currentBoard[row].length; col++) {
+      for (let row = 0; row < currentBoard.length; row =+ 1) {
+        for (let col = 0; col < currentBoard[row].length; col =+ 1) {
           if (!currentBoard[row][col]) {
             return;
-          }
+          } break;
         }
       }
       // Assign null to gameStatus.winner if the game ends in a tie
@@ -57,34 +69,34 @@ const gameController = (() => {
     };
     // Check if a player has won the game
     // Check for a "row win"
-    for (let row = 0; row < currentBoard.length; row++) {
+    for (let row = 0; row < currentBoard.length; row += 1) {
       possibleWinCombinations.push(currentBoard[row]);
     }
     // Check for a "column win"
-    for (let col = 0; col < currentBoard[0].length; col++) {
+    for (let col = 0; col < currentBoard[0].length; col += 1) {
       const extractColumns = (arr, n) => arr.map((x) => x[n]);
       possibleWinCombinations.push(extractColumns(currentBoard, col));
     }
     // Check for a "diagonal win"
     const topLeftDiag = [];
     const bottomLeftDiag = [];
-
-    for (let row = 0; row < currentBoard.length; row++) {
+    for (let row = 0; row < currentBoard.length; row += 1) {
       topLeftDiag.push(currentBoard[row][row]);
       bottomLeftDiag.push(currentBoard[row][currentBoard.length - 1 - row]);
     }
     possibleWinCombinations.push(topLeftDiag, bottomLeftDiag);
-
     // Assign the marker of the winner to the game status if there is one
     const checkForWinningCombination = (arr) => {
       arr.every((marker) => marker !== undefined && marker === arr[0]);
     };
-    for (let i = 0; i < possibleWinCombinations.length; i++) {
+
+    for (let i = 0; i < possibleWinCombinations.length; i += 1) {
       if (checkForWinningCombination(possibleWinCombinations[i])) {
+        console.log('does this run');
         gameStatus.winner = possibleWinCombinations[i][0];
       }
     }
-
+    
     checkForTie();
 
     return {
@@ -100,17 +112,24 @@ const gameController = (() => {
     const selectedColumn = prompt(
       `Hi, ${player.name}, please select a column, 0-2.`,
     );
+    // Check if the selected cell is available for play
+    if (gameBoard.show()[selectedRow][selectedColumn]) {
+        alert("That cell is already taken. Try another one.");
+        return;
+    }
     // Update the game board
-    game.updateGameBoard(player.marker, selectedRow, selectedColumn);
+    gameBoard.update(player.marker, selectedRow, selectedColumn);
     // Check if game is over
-    if (gameController.checkGameStatus().gameStatus.winner) {
+    const {winner} = gameController.checkStatus().gameStatus;
+
+    if (winner) {
       // Declare the winner
       console.log(
         `The winner plays with the marker ${
-          gameController.checkGameStatus().gameStatus.winner
+          gameController.checkStatus().gameStatus.winner
         }`,
       );
-    } else if (gameController.checkGameStatus().gameStatus.winner === null) {
+    } else if (winner === null) {
       // Declare the result as a tie
       console.log('The game is a tie!');
     } else {
@@ -119,13 +138,11 @@ const gameController = (() => {
     }
   };
 
-  // Check gameboard to see if a player has won the game
-
   const showWhoseTurn = () => whoseTurn;
 
   return {
     showWhoseTurn,
-    checkGameStatus,
+    checkStatus,
     takeTurn,
   };
 })();
